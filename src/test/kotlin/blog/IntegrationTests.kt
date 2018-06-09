fa716b3afd6edd36524903b05609f24fd40f3814
Ron Kurr <rkurr@jvmguy.com>
2018-06-09 15:33:20 +0000

    Override JUnit 5's default behavior and instantiate test classes only once, allowing us to more easily manage setup and teardown of tests.

--- src/test/kotlin/blog/IntegrationTests.kt
+++ src/test/kotlin/blog/IntegrationTests.kt
@@ -1,6 +1,8 @@
 package blog
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,10 +17,25 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @SpringBootTest( webEnvironment = RANDOM_PORT )
 class IntegrationTests( @Autowired val restTemplate: TestRestTemplate ) {
 
+    @BeforeAll
+    fun setup() {
+        println( ">> setup" )
+    }
+
+    @AfterAll
+    fun teardown() {
+        println( ">> teardown" )
+    }
+
     @Test
     fun `Assert blog page title, content and status code`() {
         val entity = restTemplate.getForEntity<String>("/")
         assertThat( entity.statusCode ).isEqualTo( HttpStatus.OK )
         assertThat( entity.body ).contains( "<h1>Blog</h1>" )
+    }
+
+    @Test
+    fun `Assert article page title, content and status code`() {
+        println(">> TODO")
     }
 }
