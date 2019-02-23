0e15cc1ecb1c69ce97f6180274cfed05b1413853
Ron Kurr <rkurr@overstock.com>
2019-02-23 20:59:10 +0000

    Added an example of how to construct a configuration object.

--- src/main/kotlin/blog/BlogApplication.kt
+++ src/main/kotlin/blog/BlogApplication.kt
@@ -4,10 +4,12 @@ import com.samskivert.mustache.Mustache
 import org.springframework.boot.Banner.Mode.OFF
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
+@EnableConfigurationProperties(BlogProperties::class)
 class BlogApplication {
     @Bean
     fun mustacheCompiler( loader: Mustache.TemplateLoader? ) = Mustache.compiler().escapeHTML( false ).withLoader( loader )!!
